f12b36721ff624a5d4fa951ba66cd44e7d730113
yk1024 <u.kato3@gmail.com>
2024-08-03 14:30:51 +0900

    クリックでインタラクトできるように

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
+    [SerializeField]
+    private Vector2 area;
+
+    private Vector3 halfExtent;
+
+    [SerializeField]
+    private float distance;
+
+    [SerializeField]
+    private Transform interactionPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        halfExtent = 0.5f * area;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked");
 
-    public void OnTest()
-    {
-        Debug.Log("TEST");
+            if (Physics.BoxCast(interactionPosition.position, halfExtent, interactionPosition.forward, out RaycastHit hit, Quaternion.identity, distance))
+            {
+                if (hit.collider.gameObject.TryGetComponent(out IInteractable interactable))
+                {
+                    Debug.Log(hit.collider.name);
+                    interactable.Interact();
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Interactable : MonoBehaviour, IInteractable
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
+        
+    }
+
+    public void Interact()
+    {
+        Debug.Log($"INTERACTING: {name}");
     }
 }
