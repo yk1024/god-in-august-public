f59483013ad5e607624eb3f249a9125d7a61ac91
yk1024 <u.kato3@gmail.com>
2024-08-15 23:15:09 +0900

    Wwiseが動くことの確認

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID MOSTCOOLEST = 864545973U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
