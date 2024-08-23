17977ab2b4b9940d22af6b333114123a2f3524fc
Yunenosei <kobakoba5884y.k@gmail.com>
2024-08-23 22:11:40 +0900

    途中です

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] anomalies;
+
+    [SerializeField]
+    private float probability;
+
     // Start is called before the first frame update
     void Start()
     {
+
+    float f = Random.value;
+    Debug.Log(f);
+
+    if (probability > f)
+    {
+
+        int i =Random.Range(0, anomalies.Length);
+        Debug.Log(i);
+        GameObject anomaly = anomalies[i];
+        anomaly.SetActive(true);
+
+    }
 
     }
 
