8691d805541335d326fda4b7bed0e7f749056f5f
Yunenosei <kobakoba5884y.k@gmail.com>
2024-08-23 14:59:13 +0900

    修正 #9

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,13 +10,13 @@ public class SceneLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void LoadScene()
