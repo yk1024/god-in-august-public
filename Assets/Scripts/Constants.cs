b97c16f33dddbd2e13f29e79a54a54931e2a9e56
yk1024 <144424308+yk1024@users.noreply.github.com>
2024-08-23 15:34:18 +0900

    日付の遷移・寝るコマンド #10 (#32)

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+public static class Constants
+{
+    public const string PlayerActionMap = "Player";
+
+    public const string UIActionMap = "UI";
+
+    public const string FadeInTrigger = "FadeIn";
+    public const string FadeOutTrigger = "FadeOut";
+}
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour, IInteractable
 {
+    [SerializeField]
+    private string interactionName;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Interact()
     {
-        Debug.Log($"INTERACTING: {name}");
+        Debug.Log($"INTERACTING: {interactionName}");
     }
 }
