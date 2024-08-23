3fd1eee0d572be82a7e0aeda67f8621822817ed3
Yunenosei <kobakoba5884y.k@gmail.com>
2024-08-24 01:32:40 +0900

    異変ランダム発生 #7 (#40)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] anomalies;
+
+    [SerializeField]
+    private float probability;
+
     // Start is called before the first frame update
     void Start()
     {
+        float f = Random.value;
 
+        if (probability > f)
+        {
+            int i = Random.Range(0, anomalies.Length);
+            GameObject anomaly = anomalies[i];
+            anomaly.SetActive(true);
+        }
     }
 
     // Update is called once per frame
