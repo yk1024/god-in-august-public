bade0f27ce0bf3d4c571c480d0f7215d4b3ea436
yk1024 <u.kato3@gmail.com>
2025-02-15 00:07:48 +0900

    アセット追加

--- Assets/TerrainSampleAssets/ShaderGraphs/CustomFunctions/CustomLighting.hlsl
+++ Assets/TerrainSampleAssets/ShaderGraphs/CustomFunctions/CustomLighting.hlsl
@@ -1,32 +1,59 @@
-void MainLight_half(float3 WorldPos, out half3 Dir, out half3 Color, out half DistanceAtten, out half ShadowAtten)
+void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
 {
-#if 1
-   Dir = half3(0.5, 0.5, 0);
-   Color = 1;
-   DistanceAtten = 1;
-   ShadowAtten = 1;
+#ifdef SHADERGRAPH_PREVIEW
+	Direction = half3(0.5, 0.5, 0);
+	Color = 1;
+	DistanceAtten = 1;
+	ShadowAtten = 1;
 #else
-   half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-   Light mainLight = GetMainLight(shadowCoord);
-   Dir = mainLight.direction;
-   Color = mainLight.color;
-   DistanceAtten = mainLight.distanceAttenuation;
-   ShadowAtten = mainLight.shadowAttenuation;
+	half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+	Light mainLight = GetMainLight(shadowCoord);
+	Direction = mainLight.direction;
+	Color = mainLight.color;
+	DistanceAtten = mainLight.distanceAttenuation;
+	
+#if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
+	ShadowAtten = 1.0h;
 #endif
-   Dir = 0;
+	ShadowAtten = mainLight.shadowAttenuation;
+#endif
 }
 
-void SampleSH_half(half3 normalWS, out half3 Ambient)
-{
-    // LPPV is not supported in Ligthweight Pipeline
-    real4 SHCoefficients[7];
-    SHCoefficients[0] = unity_SHAr;
-    SHCoefficients[1] = unity_SHAg;
-    SHCoefficients[2] = unity_SHAb;
-    SHCoefficients[3] = unity_SHBr;
-    SHCoefficients[4] = unity_SHBg;
-    SHCoefficients[5] = unity_SHBb;
-    SHCoefficients[6] = unity_SHC;
 
-    Ambient = max(half3(0, 0, 0), SampleSH9(SHCoefficients, normalWS));
+void AdditionalLights_half(half3 WorldPos, half3 WorldNormal, half3 WorldView, out half3 Diffuse)
+{
+	half3 diffuseColor = 0;
+
+#ifndef SHADERGRAPH_PREVIEW
+	WorldNormal = normalize(WorldNormal);
+	WorldView = SafeNormalize(WorldView);
+	int pixelLightCount = GetAdditionalLightsCount();
+	for (int i = 0; i < pixelLightCount; ++i)
+	{
+		Light light = GetAdditionalLight(i, WorldPos);
+		half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+		diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+	}
+#endif
+
+	Diffuse = diffuseColor;
+}
+
+
+
+void ReflectionUV_half(half2 screenUV, half3 normalWS, half3 posWS, out half2 reflectionUV)
+{
+
+	const half f = 0.02f;
+	half3 viewVec = _WorldSpaceCameraPos - posWS;
+	half3 reflNormalWS = lerp(normalWS, normalize(float3(normalWS.x * f, 1.f, normalWS.z * f)), saturate(length(viewVec.xz) / 250.f));
+	// get the perspective projection
+	float2 p11_22 = float2(unity_CameraInvProjection._11, unity_CameraInvProjection._22) * 10;
+	// conver the uvs into view space by "undoing" projection
+	float3 viewDir = -(float3((screenUV * 2 - 1) / p11_22, -1));
+
+	half3 viewNormal = mul(reflNormalWS, (float3x3)GetWorldToViewMatrix()).xyz;
+	half3 reflectVector = reflect(-viewDir, viewNormal);
+
+	reflectionUV = screenUV + reflNormalWS.zx * half2(0.05, 0.05);
 }
