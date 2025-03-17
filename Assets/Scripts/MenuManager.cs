8e92a60f53feb159fdc5874ef9dad0257b5ec579
Daruma <86365220+Daruma12345@users.noreply.github.com>
2025-03-18 02:03:55 +0900

    メニュー画面の実装 #42 (#59)

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionReference menuAction;
+
+    [SerializeField]
+    private GameObject menuPanel;
+
+    [SerializeField]
+    private GameObject aboutGamePanel;
+
+    [SerializeField]
+    private GameObject controlPanel;
+
+    [SerializeField]
+    private GameObject creditPanel;
+
+    private GameObject[] panels;
+
+    void Awake()
+    {
+        menuAction.action.performed += OpenMenu;
+        panels = new GameObject[] { menuPanel, aboutGamePanel, controlPanel, creditPanel };
+    }
+
+    void OnDestroy()
+    {
+        menuAction.action.performed -= OpenMenu;
+    }
+
+    private void OpenMenu(InputAction.CallbackContext context)
+    {
+        OpenMenu();
+    }
+
+    public void OpenMenu()
+    {
+        menuPanel.SetActive(true);
+    }
+
+    public void ToOtherPanel(GameObject panel)
+    {
+        CloseAllPanels();
+        panel.SetActive(true);
+    }
+
+
+    private void CloseAllPanels()
+    {
+        foreach (var p in panels) { p.SetActive(false); }
+    }
+}
---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
