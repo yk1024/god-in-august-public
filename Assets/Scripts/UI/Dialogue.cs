980eeec7df3d078eee8fad5b342c680070e10bac
yk1024 <u.kato3@gmail.com>
2025-03-22 04:18:30 +0900

    文字送りの音・ダッキング

--- Assets/Scripts/UI/Dialogue.cs
+++ Assets/Scripts/UI/Dialogue.cs
@@ -18,6 +18,12 @@ public class Dialogue : SingletonBehaviour<Dialogue>
     [SerializeField, Tooltip("文字送りのアクション")]
     private InputActionReference toNextAction;
 
+    [SerializeField, Tooltip("文字を表示する際の音を再生するイベント")]
+    private AK.Wwise.Event nextCharacterEvent;
+
+    [SerializeField, Tooltip("次の文章を表示する際の音を再生するイベント")]
+    private AK.Wwise.Event nextTextEvent;
+
     // 次の文字が表示されるまでの時間
     private const float TimePerCharacter = 0.1f;
 
@@ -46,6 +52,9 @@ public class Dialogue : SingletonBehaviour<Dialogue>
                 // ダイアログに次の文字を追記する。
                 textMeshProUGUI.text += character;
 
+                // SFX再生
+                nextCharacterEvent.Post(gameObject);
+
                 // 次の文字が表示されるまで待つ。
                 yield return new WaitForSeconds(TimePerCharacter);
             }
@@ -55,6 +64,9 @@ public class Dialogue : SingletonBehaviour<Dialogue>
 
             // 次の文に進めるための入力をプレイヤーが行うまで待つ。
             yield return new WaitUntil(() => toNextAction.action.IsPressed());
+
+            // SFX再生
+            nextTextEvent.Post(gameObject);
         }
 
         // 元のアクションマップに戻し、アクティブの状態も戻す。
--- Assets/Scripts/UI/UIPanel.cs
+++ Assets/Scripts/UI/UIPanel.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using GodInAugust.System;
 
 namespace GodInAugust.UI
 {
